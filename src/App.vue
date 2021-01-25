c4bafc2cabb689bb28a871995b1a7a423bcfd4c5
DJLauwy <laudehoop@hotmail.com>
2021-01-25 15:47:59 +0100

    Beginnetje Vue
    title says it all :)

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,56 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+
+  <h1>Bucketlist</h1>
+
+  <form @submit.prevent="voegLifeGoal">
+    <label>Voeg een nieuw life-goal toe</label>
+      <input v-model="lifeGoal" name="lifeGoal">
+      <button>Toevoegen</button>
+  </form>
+  <ul>
+    <li v-for="goal in goals" :key="todo.id">
+      <h3 @click="toggleDone(goal)">{{goal.content}}</h3>
+    </li>
+  </ul>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  }
+
+  setup(){
+
+    const lifeGoal = ref('');
+    const goals = ref([]);
+
+    function voegLifeGoal(){
+      goals.value.push({
+        id: new Date.now(),
+        done: false,
+        content: lifeGoal.value,
+      });
+      lifeGoal.value = '';
+    }
+
+    function toggleDone(goal){
+      goal.done = !goal.done;
+    }
+
+    return {
+      goals,
+      lifeGoal,
+      voegLifeGoal,
+      toggleDone,
+    };
+
   }
 }
 </script>
@@ -24,5 +65,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.done{
+  text-decoration:line-through;
 }
 </style>
